4d3c7af77de3cf775fe186588aead38973818765
BreinerJT <breinerjtorres@gmail.com>
2022-07-08 23:10:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const toggleBtn = document.querySelector(".toggle-btn")
 
+let isDark = localStorage.getItem("dark-theme")
+
+const enableDarkMode = () => {
+    document.body.classList.add("dark-theme")
+    localStorage.setItem("dark-theme", "enabled")
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("dark-theme")
+    localStorage.setItem("dark-theme", null)
+}
+
+if(isDark === "enabled") document.body.classList.add("dark-theme")
+
 toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme")
+    isDark = localStorage.getItem("dark-theme")
+    if(isDark !== "enabled") enableDarkMode()
+    else disableDarkMode()
 })
